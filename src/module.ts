5dff57201086b4c37b4ced5781bee9260d45f3ca
Jack Anderson <hi@janderson.me>
2020-07-02 00:35:24 -0700

    no side-effects of create-worker

--- src/module.ts
+++ src/module.ts
@@ -1,23 +1,15 @@
-import { TWorkerImplementation, createWorker } from 'worker-factory';
-import { IMidiJsonParserWorkerCustomDefinition } from './interfaces';
-import { parseArrayBuffer } from './midi-file-parser';
+import { TWorkerImplementation, createWorker } from "worker-factory";
+import { IMidiJsonParserWorkerCustomDefinition } from "./interfaces";
+import { parseArrayBuffer } from "./midi-file-parser";
 
 /*
  * This export can provide Node.js support
  */
-export { parseArrayBuffer } from './midi-file-parser';
+export { parseArrayBuffer } from "./midi-file-parser";
 
 /*
  * @todo Explicitly referencing the barrel file seems to be necessary when enabling the
  * isolatedModules compiler option.
  */
-export * from './interfaces/index';
-export * from './types/index';
-
-createWorker<IMidiJsonParserWorkerCustomDefinition>(self, <TWorkerImplementation<IMidiJsonParserWorkerCustomDefinition>>{
-    parse: ({ arrayBuffer }) => {
-        const midiFile = parseArrayBuffer(arrayBuffer);
-
-        return { result: midiFile };
-    }
-});
+export * from "./interfaces/index";
+export * from "./types/index";
